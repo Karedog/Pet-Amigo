ada0879a1480a6137530c3be4880262a418daa8e
Douglas <douglasdanielx@gmail.com>
2026-01-19 20:12:57 -0300

    feat: cria o componente banner e adiciona css

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,14 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "@/components/Header";
 import Banner from "@/components/Banner";
+import Servicos from "@/components/Servicos";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <Header/>
       <Banner/>
+      <Servicos/>
     </div>
   );
 }
--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,5 +1,5 @@
 import estilo from "./index.module.css"
-import { CiStar } from "react-icons/ci";
+import { FaStar } from "react-icons/fa";
 import { CiCalendar } from "react-icons/ci";
 import { BsTelephone } from "react-icons/bs";
 import { CiCircleCheck } from "react-icons/ci";
@@ -9,18 +9,20 @@ import Image from "next/image";
 
 export default function Banner(){
     return(
-        <section>
+        <section className={estilo.secaoBanner}>
             <div className={estilo.Banner}>
                 <div className={estilo.BannerTexto}>
-                    <p className={estilo.textoTopo}><CiStar/> Votado o melhor petshop da cidade</p>
+                    <p className={estilo.textoTopo}><FaStar  className={estilo.estrela}/> Votado o melhor petshop da cidade</p>
                     <p className={estilo.textoDestaque}>Amor e cuidado que seu melhor amigo <span>merece</span>.</p>
                     <p className={estilo.texto}>Banho, tosa, veterinária e day care com profissionais apaixonados por animais. Seu pet vai se sentir em casa.</p>
-                    <div>
-                        <button><CiCalendar /> Agendar Horario</button>
-                        <button><BsTelephone/> (11) 99999-9999</button>
+                    <div className={estilo.caixaButton}>
+                        <button className={`${estilo.button} ${estilo.buttonAgendar}`}><CiCalendar className={estilo.CiCalendar} /> Agendar Horário</button>
+                        <button className={`${estilo.button} ${estilo.buttonTelefone}`}><BsTelephone className={estilo.BsTelephone}/> (11) 99999-9999</button>
                     </div>
-                    <p className={estilo.textoCheck}><CiCircleCheck/> Profissionais Certificados</p>
-                    <p className={estilo.textoCheck}><CiCircleCheck/> Ambiente Monitorado</p>
+                    <div className={estilo.caixaTextoCheck}>
+                        <p className={estilo.textoCheck}><CiCircleCheck className={estilo.CiCircleCheck}/> Profissionais Certificados</p>
+                        <p className={estilo.textoCheck}><CiCircleCheck className={estilo.CiCircleCheck}/> Ambiente Monitorado</p>
+                    </div>
                 </div>
                 <div className={estilo.BannerImagem}>
                     <Image src={cachorro}/>
